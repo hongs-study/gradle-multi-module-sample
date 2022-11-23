afd485ad7f7c31272f38ee82296ec0c0b8de94e8
aisha <aisha@gccompany.co.kr>
2022-11-23 15:40:50 +0900

    [queryDsl설정] entity모듈에 설정

---
+++ shop-api/src/main/java/com/example/ShopRepository.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import com.example.shopentity.member.entity.QMember;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ShopRepository {
+
+    private QMember member = QMember.member;
+
+}
